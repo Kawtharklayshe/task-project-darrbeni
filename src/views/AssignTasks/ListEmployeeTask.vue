14fa0d0d983e2b80086393694b7bfaa8a53ce619
Nagham shaheen <naghamshaheen019@gmail.com>
2023-11-23 09:34:35 +0200

    first nagham commit

---
+++ src/views/AssignTasks/ListEmployeeTask.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <AssignTaskEmployee  @getitems="retrieveFromLocalStorage"/>
+        <b-table striped hover :items="items"></b-table>
+    </div>
+</template>
+<script>
+import  { BTable } from "bootstrap-vue";
+import AssignTaskEmployee from "./AssignTaskEmployee.vue";
+export default {
+   components:{
+        BTable,
+        AssignTaskEmployee
+    },
+ data() {
+      return {
+       items:[],
+        }
+    },
+    methods: {
+        retrieveFromLocalStorage() {
+        const saved = localStorage.getItem("items");
+        if (saved) {
+          this.items = JSON.parse(saved);
+        }
+        },
+
+    }}
+
+</script>
