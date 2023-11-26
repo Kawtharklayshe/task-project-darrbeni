a733dfc7e8f6a3d6dfcaeeb323fc3816a674c1ea
Mohammed Tammas <145572106+mohammed-tammas@users.noreply.github.com>
2023-11-26 10:33:04 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css' // Import Bootstrap CSS
+import 'bootstrap-vue/dist/bootstrap-vue.css' // Import Bootstrap-Vue CSS
+// npm 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import { TableComponent } from 'vue-table-component'
+
+
+
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue) // Register BootstrapVue
+Vue.use(IconsPlugin)
+Vue.use(TableComponent)
+
+
+new Vue({
+  router,
+  store,
+  render: function (h) { return h(App) }
+}).$mount('#app')
